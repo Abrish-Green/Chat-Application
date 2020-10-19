163d5df4a551df43e3623752488d248194bb9d22
Abrish-Green <abrham365muche@gmail.com>
2020-10-19 17:24:53 +0300

    Added Loading Stuff

--- public/js/chatRoom.js
+++ public/js/chatRoom.js
@@ -1,13 +1,16 @@
 var socket = io()
-var messages = jQuery('ol')
+var messages = jQuery('ul')
 
 socket.on('newUser', (message) => {
     console.log(`${message.text} from ${message.from}`)
     console.log(message)
-    messages.append(`<li> ${message.from}: ${message.text}</li>`)
+   
+    
+    //messages.append(`<li><b style="color:green">${message.from}</b> : ${message.text} </li><b class="time" style="font-size:10px;">${message.time}</b>`)
 })
 
-jQuery('#send').on('click', function (e) { 
+
+jQuery('#message-form').on('submit', function (e) { 
     e.preventDefault()
     console.log(jQuery("#message").val())
 
@@ -16,6 +19,7 @@ jQuery('#send').on('click', function (e) {
         text: jQuery('[name=message]').val(),
       
     })
+    
 })
 
 jQuery('#ip').on('click', function(e) {
@@ -26,10 +30,28 @@ jQuery('#ip').on('click', function(e) {
 
 socket.on('getIP', function (message) {
     console.log(message.ip)
-    messages.append(`<li> ${message.from}: ${message.ip}</li>`)
+    messages.append(`<li><b> ${message.from}</b><b class="time">${message.time}</b> ${message.ip}</li>`)
     console.log('clicked')
 
     })
 socket.on('groupChat', function(message){
-    messages.append(`<li> ${message.from}: ${message.text}</li>`)
+    messages.append(`<li><b> ${message.from}</b><b class="time">${message.time}</b> </li><li class="messages">${message.text}</li>`)
+   
 })
+
+function autoUpdate() { 
+    
+        socket.on('autoReload', function (message) { 
+            jQuery('#users').html('')
+             message.all_users.forEach(function (user) {
+             let  tempUser =  (user.substr(0, 3) + '@User').toUpperCase()
+            jQuery('#users').append(`<li class="users-class">${tempUser}</li>`)
+        })
+        })
+    console.log("reloading...")
+    }
+
+
+setInterval(autoUpdate,1000)
+
+
--- server/chatRoomServer.js
+++ server/chatRoomServer.js
@@ -4,9 +4,11 @@ const jQuery = require('jquery')
 const http = require('http')
 const socketIO = require('socket.io')
 const ip = require('ip')
+const moment = require('moment')
 const PORT =process.env.PORT || 8001
 const app = express()
 
+const users = []
 const ipAddress =  ip.address()
 app.use((req, res, next) => {
     console.log(req.method + " "+ req.url)
@@ -23,16 +25,25 @@ app.use(express.static(publicPath))
 const io = socketIO(server)
 
 io.on('connection', (socket) => {
+    users.push(socket.id)
 
+    setInterval(() => {
+       socket.emit('autoReload', {
+         all_users: users
+    }) 
+    },1000)
+    
     socket.emit('newUser', {
         from: 'Admin',
         text: 'Welcome to the Chat App',
-        createdAt: new Date().getTime()
+        time: moment().format(' h:mm a'),
+        all_users: users
     })
     socket.broadcast.emit('newUser', {
         from: 'Admin',
         text: 'User Joined',
-        createdAt: new Date().getTime()
+        time: moment().format(' h:mm a'),
+        all_users: users
     })
     socket.on('createMessage', (user) => {
         console.log(user)
@@ -40,28 +51,37 @@ io.on('connection', (socket) => {
         socket.emit('groupChat', {
             from: 'Me',
             text: user.text,
+            time: moment().format(' h:mm a'),
+             all_users: users
             
             
         })
         socket.broadcast.emit('groupChat', {
             from: 'Anonmyous',
             text: user.text,
+            time: moment().format(' h:mm a'),
+             all_users: users
         })
     })
     socket.on('ip', () => {
             socket.emit('getIP', {
                 from: 'Me',
-                ip: ipAddress
+                ip: ipAddress,
+                time: moment().format(' h:mm a')
             })
              socket.broadcast.emit('getIP', {
                 from: 'Anonmyous',
-                ip: ipAddress
-                })
+                ip: ipAddress,
+                time: moment().format(' h:mm a')
+            })
         })
     socket.on('disconnect', () => {
         console.log('Client Disconnected')
+        users.pop(socket.id)
     })
 });
+
+//console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
 server.listen(PORT, () => {
     console.log('Server is Up at Port : ',PORT)
